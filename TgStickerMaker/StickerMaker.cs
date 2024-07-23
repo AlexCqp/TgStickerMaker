c7731a662ae1f6be0102c59da0fc51c9e31801f3
Aleksandr Chernyshov <acqp2tl@gmail.com>
2024-07-23 23:55:37 +0300

    for test 3

--- TgStickerMaker/StickerMaker.cs
+++ TgStickerMaker/StickerMaker.cs
@@ -41,7 +41,7 @@ namespace TgStickerMaker
                 Directory.CreateDirectory(videosPath);
             }
 
-            string ffmpegPath = Path.Combine(AppContext.BaseDirectory, "ffmpeg");
+            string ffmpegPath = Path.Combine(AppContext.BaseDirectory, "/usr/bin/ffmpeg");
             FFmpeg.SetExecutablesPath(Path.GetDirectoryName(ffmpegPath));
             
             var outputFilePath = WriteFilesHelper.GetUniqueFileName(Path.Combine(videosPath, "output.webm"));
@@ -84,7 +84,7 @@ namespace TgStickerMaker
                 Directory.CreateDirectory(videosPath);
             }
 
-            string ffmpegPath = Path.Combine(AppContext.BaseDirectory, "ffmpeg");
+            string ffmpegPath = Path.Combine(AppContext.BaseDirectory, "/usr/bin/ffmpeg");
             FFmpeg.SetExecutablesPath(Path.GetDirectoryName(ffmpegPath));
 
             var outputFilePath = WriteFilesHelper.GetUniqueFileName(Path.Combine(videosPath, "output.webm"));
